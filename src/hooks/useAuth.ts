ec39d984ff7c26d3dc36f26550c576138b0391c3
Berra Karaman <esma.berra.karaman@hotmail.com>
2025-05-08 14:38:48 +0300

    create react query hook for auth

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,59 @@
+import authService from "@/service/auth/auth";
+import { LoginRequest, SignupRequest } from "@/service/auth/auth.types";
+import { addToast } from "@heroui/toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+
+export const useSignup = () => {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  return useMutation({
+    mutationKey: ["signup"],
+    mutationFn: (data: SignupRequest) => {
+      return authService.signup(data);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["signup"] });
+      router.push("/dashboard");
+      addToast({
+        title: "Signup successfull",
+        color: "success"
+      });
+    },
+    onError: (error) => {
+      addToast({
+        title: "Signup failed.",
+        description: error.message,
+        color: "danger",
+      });
+    }
+  })
+}
+
+export const useLogin = () => {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  return useMutation({
+    mutationKey: ["login"],
+    mutationFn: (data: LoginRequest) => {
+      return authService.login(data);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["login"] });
+      router.push("/dashboard");
+      addToast({
+        title: "Login successfull",
+        color: "success"
+      });
+    },
+    onError: (error) => {
+      addToast({
+        title: "Login failed",
+        description: error.message,
+        color: "danger",
+      });
+    }
+  })
+}
